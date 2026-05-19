272c7e2f20f151070ec9c577e3b50d1dc4274a37
Franco3813 <franconicolasaguirre.04@gmail.com>
2026-05-19 12:11:18 -0300

    punto 2 incompleto

--- Program.cs
+++ Program.cs
@@ -4,14 +4,16 @@ Console.WriteLine("Hello, World!");
 int a;
 int b;
 
-a = 10;
+a = 0;
 b = 20;
 
 Console.WriteLine("El valo de a:"+a);
 Console.WriteLine("El valor de b:"+b);
 
 int i = 0;
-string dato = "10";
+string dato;
+Console.WriteLine("Ingrese un numero: ");
+dato = Console.ReadLine();
 
 if(int.TryParse(dato, out i) && i > 0){
     int aux = 0;
@@ -22,4 +24,13 @@ if(int.TryParse(dato, out i) && i > 0){
     Console.WriteLine("Funciona o nel "+aux +i);
 }
 
+do{
+    Console.WriteLine("Ingrese una operacion: \n1)Suma \n2)Resta \n3)Multiplicacion \n4)Divicion");
+    string c;
+    c = Console.ReadLine();
+
+    
+    Console.WriteLine("El valo de a:"+c);
+
+}while(a == 1);
 
