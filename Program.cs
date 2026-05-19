78f8fa3f557c35fad149a15cb13e02e6d27df5a7
Franco3813 <franconicolasaguirre.04@gmail.com>
2026-05-19 11:46:24 -0300

    Ejercicio 1

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int a;
+int b;
+
+a = 10;
+b = 20;
+
+Console.WriteLine("El valo de a:"+a);
+Console.WriteLine("El valor de b:"+b);
+
+int i = 0;
+string dato = "10";
+
+if(int.TryParse(dato, out i) && i > 0){
+    int aux = 0;
+    while(i >= 10){
+        aux  = aux*10 + i%10;
+        i = i/10;
+    }
+    Console.WriteLine("Funciona o nel "+aux +i);
+}
+
+
