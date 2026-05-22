07d6798c0608aa111d3ebb50f01956087158c15d
Franco3813 <franconicolasaguirre.04@gmail.com>
2026-05-22 18:20:36 -0300

    punto 2 completo

--- Program.cs
+++ Program.cs
@@ -1,11 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
 
 int a;
 int b;
 
-a = 0;
-b = 20;
+a = 10;
+b = a;
 
 Console.WriteLine("El valo de a:"+a);
 Console.WriteLine("El valor de b:"+b);
@@ -21,16 +20,39 @@ if(int.TryParse(dato, out i) && i > 0){
         aux  = aux*10 + i%10;
         i = i/10;
     }
-    Console.WriteLine("Funciona o nel "+aux +i);
+    Console.WriteLine("El numero invertido es: "+aux +i);
 }
 
+string c,d,e,x;
+int f,g,h;
 do{
+    
     Console.WriteLine("Ingrese una operacion: \n1)Suma \n2)Resta \n3)Multiplicacion \n4)Divicion");
-    string c;
     c = Console.ReadLine();
 
+    Console.WriteLine("Ingrese el primer numero: ");
+    d = Console.ReadLine();
+    Console.WriteLine("Ingrese el segundo numero: ");
+    e = Console.ReadLine();
     
-    Console.WriteLine("El valo de a:"+c);
+    if(int.TryParse(d, out f) && int.TryParse(e, out g)){
 
-}while(a == 1);
+        if(c == "1"){
+            h = f+g;
+            Console.WriteLine("La suma es: "+h);
+        }else if(c == "2"){
+            h = f-g;
+            Console.WriteLine("La resta es: "+h);
+        }else if(c == "3"){
+            h = f*g;
+            Console.WriteLine("La multiplicacion es: "+h);
+        }else if(c == "4" && g != 0){
+            h = f/g;
+            Console.WriteLine("La divicion es: "+h);
+        }
+    }
 
+    Console.WriteLine("Desea hacer otra operacion? \n1)Si \n2)No");
+    x = Console.ReadLine();
+    
+}while(x == "1");
